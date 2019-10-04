536f39e053d909770723f2196a27e0740bd2fecd
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-10-04 11:04:01 -0700

    refactored number guesser

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 /* eslint-disable no-console */
 import compareNumber from './function.js';
 const myButton = document.getElementById('guessButton');
-const correctNumber = Math.floor(Math.random() * 10 + 1);
+const correctNumber = Math.ceil(Math.random() * 10);
 let remainingGuesses = 3;
-const theGuess = document.getElementById('guessField');
+let resultActual;
 
-myButton.onclick = () => {
-    const guess = theGuess.value;
-    if ((compareNumber(guess, correctNumber) === 0) && (remainingGuesses > 0)) {
-        console.log(correctNumber);
-        console.log(guess);
-        alert('You win! Please enjoy this complimentary glory!');
-        remainingGuesses = 0;
-    } else if ((compareNumber(guess, correctNumber) === -1) && (remainingGuesses > 0)) {
-        remainingGuesses -= 1;
-        alert('That number is too low! You have ' + remainingGuesses + ' guesses left...');
-        console.log(correctNumber);
-        console.log(guess);
-    } else if ((compareNumber(guess, correctNumber) === 1) && (remainingGuesses > 0)){
-        remainingGuesses -= 1;
-        alert('That number is too high! You have ' + remainingGuesses + ' guesses left!');
-        console.log(correctNumber);
-    } else {
-        alert('That\'s the end of the game, dude. It\'s not a very long one, sorry.');
-    }
+const getGuess = () => {
+    const theGuess = document.getElementById('guessField');
+    return theGuess.value;
 };
+
+const trackGuesses = (result) => {
+    if (result === -1 || result === 1){
+        remainingGuesses -= 1;
+    } else {
+        remainingGuesses = 0;
+    }
+
+const gameOver = () => {
+    if (remainingGuesses === 0) {
+        alert('That\'s the end of the game, dude. It\'s not a very long one, sorry.');
+        return;
+    } 
+}
+
+const alertUser = (result) => {
+    if (result === -1){
+        alert('That number is too low! You have ' + remainingGuesses + ' guesses left...');
+    } else if (result === 1) {
+        alert('That number is too high! You have ' + remainingGuesses + ' guesses left!');
+    } else if (results === 0) {
+        alert('You win! Please enjoy this complimentary glory!');
+    } 
+}
+    
+const playRound = () => {
+    getGuess();
+    resultActual = compareNumber(getGuess(), correctNumber);
+    trackGuesses(resultActual);
+    gameOver();
+    alertUser(resultActual);
+}
+myButton.onclick = () => {playRound()}
